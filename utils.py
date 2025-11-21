248ae6e42d5bd576ba70220c5bad08ca806bc19d
maniliy07 <56462904+maniliy07@users.noreply.github.com>
2025-11-20 18:05:51 -0800

    Resume Evaluation Generative AI Project
    Capstone Project for streamlit

---
+++ utils.py
@@ -0,0 +1,12 @@
+#utils
+import re
+
+def sanitize_text(s):
+    return " ".join(s.split())
+
+def chunk_text(s, n=2000):
+    words = s.split()
+    chunks = []
+    for i in range(0, len(words), n):
+        chunks.append(" ".join(words[i:i+n]))
+    return chunks
