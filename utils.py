7e97273d774739dbf6c76a0ed86f142040d7444e
maniliy07 <56462904+maniliy07@users.noreply.github.com>
2025-12-31 22:22:41 -0800

    utils file updated with comments

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 #utils
-import re
+import re # regex operations
 
-def sanitize_text(s):
+def sanitize_text(s): # sanitize text by removing extra spaces and newlines
     return " ".join(s.split())
 
-def chunk_text(s, n=2000):
+def chunk_text(s, n=2000): # chunk text into n-word pieces
     words = s.split()
     chunks = []
     for i in range(0, len(words), n):
